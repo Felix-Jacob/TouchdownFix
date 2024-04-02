682dae9fe37b9364ef59019fdfa46bc6c24ab3bf
Felix-Jacob <149216340+Felix-Jacob@users.noreply.github.com>
2024-04-02 15:28:54 -0400

    Update links.js

--- Links/links.js
+++ Links/links.js
@@ -1,9 +1,11 @@
 const linksUi= document.getElementById('ui');
-const pass= document.getElementById('pass');
+const string2= document.getElementById('pass');
 const button1= document.querySelector('#button1');
 const inputSpan= document.getElementById('inputSpan');
+const string1="TOCH24$"
 function displayScreen() {
-if (pass.value="TOCH24$") {
+    console.log(string2.value)
+if (string1===string2.value) {
 linksUi.style.display="block";
 inputSpan.style.display="none";
 } else {alert("Password Incorrect")}
